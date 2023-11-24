e6d53586665ff80cda59fab3b41e494028da16e8
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-24 08:50:51 -0500

    Variables para filas y columnas de matrix agregadas

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int n, m;
+
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &m);
+
+    int matriz[n][m];
+    
+    return 0;
+}
